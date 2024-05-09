04acc9ab455f8af34435111ce664c60f40451e97
GvMorais1 <112278477+GvMorais1@users.noreply.github.com>
2024-05-09 15:12:13 +0000

    aula ionic

---
+++ projeto1/src/app/home/home.page.ts
@@ -0,0 +1,27 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-home',
+  templateUrl: 'home.page.html',
+  styleUrls: ['home.page.scss'],
+})
+
+
+export class HomePage {
+  valor1: string;
+  valor2: string;
+  resultado: number;
+
+  constructor() {
+    this.valor1 = "0";
+    this.valor2 = "0";
+    this.resultado = 0;
+  }
+    calcular() {
+      this.resultado = parseFloat(this.valor1) + parseFloat(this.valor2);
+    }
+    exibirResultado():number {
+      return this.resultado;
+    }
+  }
+
